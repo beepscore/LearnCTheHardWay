71035db93834f55d74644877bd7577f3ca7d05f1
beepscore <support@beepscore.com>
2013-12-23 23:04:32 -0800

    Add comments with references for C escape codes and format specifiers.

--- ex3.c
+++ ex3.c
@@ -5,6 +5,11 @@ int main()
     int age = 10;
     int height = 72;
 
+    // c escape sequences
+    // http://www.phys.unsw.edu.au/~mcba/phys2020/notes/escape.html
+    // format specifiers
+    // http://www.cprogramming.com/tutorial/printf-format-strings.html
+
     printf("I am %d years old.\n", age);
     printf("I am %d inches tall.\n", height);
 
