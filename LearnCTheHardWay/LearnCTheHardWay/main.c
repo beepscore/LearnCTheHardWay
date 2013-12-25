702ea71413030b5737f92558224cddede643e7e9
beepscore <support@beepscore.com>
2013-12-24 22:48:23 -0800

    In Xcode create OS X application command line tool named LearnCTheHardWay.

---
+++ LearnCTheHardWay/LearnCTheHardWay/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LearnCTheHardWay
+//
+//  Created by Steve Baker on 12/24/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
