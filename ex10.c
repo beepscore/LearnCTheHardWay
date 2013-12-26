5b3a00b6a066add93afc1b28a00d43dc7e3b4009
beepscore <support@beepscore.com>
2013-12-26 11:42:47 -0800

    In ex10.c add comments.

--- ex10.c
+++ ex10.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// OS sets argc to count of elements in argv array
 int main(int argc, char *argv[])
 {
     int i = 0;
@@ -13,7 +14,18 @@ int main(int argc, char *argv[])
         printf("arg %d: %s\n", i, argv[i]);
     }
 
-    // let's make our won array of strings
+    // let's make our own array of strings
+    // each char * string is an array, and states is an array of arrays
+    // state index |  string index
+    // ----------- |  ------------
+    //           0 |  0123456789
+    //             |  California
+    //           1 |  012345
+    //             |  Oregon
+    //           2 |  0123456789
+    //             |  Washington
+    //           3 |  01234
+    //             |  Texas
     char *states[] = {
         "California", "Oregon",
         "Washington", "Texas"
