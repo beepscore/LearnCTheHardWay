41dc1e60d0de964817c9c1f14d5c9c517d4fc110
beepscore <support@beepscore.com>
2013-12-26 23:03:59 -0800

    In ex14.c do extra credit. Add print_digits() and print_digit()

--- ex14.c
+++ ex14.c
@@ -4,26 +4,48 @@
 
 // forward declarations
 bool can_print_it(char ch);
+void print_digits(char arg[]);
 void print_letters(char arg[]);
 void print_letter(char ch);
+void print_digit(char ch);
 
 void print_arguments(int argc, char *argv[])
 {
     int i = 0;
 
+    printf("print letters\n");
     for (i = 0; i < argc; i++) {
         print_letters(argv[i]);
     }
+
+    printf("\n");
+
+    printf("print digits\n");
+    for (i = 0; i < argc; i++) {
+        print_digits(argv[i]);
+    }
 }
 
-/** for each character in string argument,
- *  prints character and ASCII integer code
+/** for each letter in string argument,
+ *  prints letter and ASCII integer code
  */
 void print_letters(char arg[])
 {
     int i = 0;
     for (i = 0; arg[i] != '\0'; i++) {
         print_letter(arg[i]);
+    }
+    printf("\n");
+}
+
+/** for each digit in string argument,
+ *  prints digit and ASCII integer code
+ */
+void print_digits(char arg[])
+{
+    int i = 0;
+    for (i = 0; arg[i] != '\0'; i++) {
+        print_digit(arg[i]);
     }
     printf("\n");
 }
@@ -36,7 +58,7 @@ bool can_print_it(char ch)
     return isalpha(ch) || isblank(ch);
 }
 
-/** print letter if it is alpha or blank
+/** print character if it is alpha or blank
  *  else don't print
  */
 void print_letter(char ch)
@@ -46,8 +68,19 @@ void print_letter(char ch)
     }
 }
 
+/** print character if it is a digit
+ *  else don't print
+ */
+void print_digit(char ch)
+{
+    if (isdigit(ch)) {
+        printf("'%c' == %d ", ch, ch);
+    }
+}
+
 int main(int argc, char *argv[]) {
     // note syntax is argv not argv[]
     print_arguments(argc, argv);
+
     return 0;
 }
