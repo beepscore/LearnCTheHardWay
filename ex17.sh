1f5750da64e91c9f209f73c2c14e19d046bf5f8c
beepscore <support@beepscore.com>
2013-12-29 12:58:20 -0800

    Add bash script ex17.sh to run commands on program ex17.
    To make file executable, in terminal $ chmod u+x ex17.sh

---
+++ ex17.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# for use with Learn C The Hard Way ex17.c
+
+# connect to database file. If it doesn't exist, create it.
+./ex17 db.dat c
+printf "add records\n"
+# add record with id 1
+./ex17 db.dat s 1 zed zed@zedshaw.com
+# add record with id 2
+./ex17 db.dat s 2 frank frank@zedshaw.com
+./ex17 db.dat s 3 joe joe@zedshaw.com
+
+printf "list database\n"
+./ex17 db.dat l
+
+printf "delete record id 3\n"
+./ex17 db.dat d 3
+
+printf "list database\n"
+./ex17 db.dat l
+
+printf "get record id 2\n"
+./ex17 db.dat g 2
